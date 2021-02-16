42daebcc9d069dfd47986992add496d30b787302
asma20 <jbeli.asma20@gmail.com>
2021-02-16 12:13:30 +0100

    variable boolean

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Partie2_Exercice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean rainy=false;
+
+            if (rainy == false)
+            {
+                Console.WriteLine("Bonjour, il ne pleut pas aujourd’hui, bonne journée.");
+
+            }
+            else {
+                Console.WriteLine(" Bonjour, il pleut aujourd’hui, vous devriez prendre votre parapluie.");
+
+            }
+        }
+    }
+}
